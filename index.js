a7718986cb1cd15325b83765da78af23d2131d3d
Victor Shepardson <victor.shepardson@gmail.com>
2017-09-28 23:56:44 -0400

    feedback doodle

--- index.js
+++ index.js
@@ -9,31 +9,45 @@ const drawFeedback = regl({
   uniform sampler2D texture;
   uniform vec2 mouse;
   uniform float t;
+  uniform vec2 size;
   const float pi = 3.14159;
-  varying vec2 uv;
+  // varying vec2 uv;
   vec2 displace(vec3 c){
-    return c.rg*c.b*1./3.;
+    return c.rg*exp(2.*(1.-c.b));
+  }
+  vec3 samp(vec2 p){
+    return texture2D(texture, p/size).rgb;
   }
   void main () {
-    float dist = length(gl_FragCoord.xy - mouse);
-    vec3 d = exp(-uv.r*vec3(1., 2., 3.));//min(exp(-dist*vec3(1e-3, 1.5e-3, 2e-3)), 1.);
-    vec3 c0 = texture2D(texture, uv).rgb;
+    vec2 uv = gl_FragCoord.xy;
+    float dist = length(gl_FragCoord.xy - mouse)/size.y;
+    vec3 src = 0.4*uv.y/size.y-exp(-dist*.5*vec3(2, 3, 1)); //exp(-uv.x/size.x*vec3(1., 1.5, 2.))-exp(-dist*vec3(2, 3, 1));
+    vec3 px = vec3(1.,1.,0.);
+    vec3 c0 = samp(uv);
+    vec3 cb = (4.*c0
+      + samp(uv+px.xz)
+      + samp(uv-px.xz)
+      + samp(uv+px.zy)
+      + samp(uv-px.zy)
+    )/8.;
     vec3 c = c0-0.5;
-    c = (c+texture2D(texture, fract(uv+displace(c.rgb))).rgb-0.5);
-    c = (c+texture2D(texture, fract(uv+displace(c.gbr))).rgb-0.5);
-    c = (c+texture2D(texture, fract(uv+displace(c.brg))).rgb-0.5);
-    c = (c+texture2D(texture, fract(uv+displace(c.rbg))).rgb-0.5);
-    c = (c+texture2D(texture, fract(uv+displace(c.grb))).rgb-0.5);
-    c = (c+texture2D(texture, fract(uv+displace(c.bgr))).rgb-0.5);
-    gl_FragColor = vec4(mix(c0, fract(c.gbr+d), exp(-2.3-1.2*sin(2.*pi*t/20.))),1.);//0.5*min(exp(-dist*1e-2), 1.)), 1.);
+    float d = (1.+dist);
+    c = (c+samp(uv+d*displace(c.rgb))-0.5);
+    c = (c+samp(uv+d*displace(c.gbr))-0.5);
+    c = (c+samp(uv+d*displace(c.brg))-0.5);
+    c = (c+samp(uv+d*displace(c.rbg))-0.5);
+    c = (c+samp(uv+d*displace(c.grb))-0.5);
+    c = (c+samp(uv+d*displace(c.bgr))-0.5);
+    vec3 c1 = mix(c0, cb, 0.1);
+    gl_FragColor = vec4(mix(c1, fract(c.gbr+src), exp(-2.3-1.2*sin(2.*pi*t/20.))),1.);
   }`,
 
   vert: `
   precision mediump float;
   attribute vec2 position;
-  varying vec2 uv;
+  // varying vec2 uv;
   void main () {
-    uv = position;
+    // uv = position;
     gl_Position = vec4(2.0 * position - 1.0, 0, 1);
   }`,
 
@@ -50,7 +64,9 @@ const drawFeedback = regl({
       mouse.x * pixelRatio,
       viewportHeight - mouse.y * pixelRatio
     ],
-    t: ({tick}) => 0.01 * tick
+    t: ({tick}) => 0.01 * tick,
+    // size: ({viewportWidth, viewportHeight}) => [viewportWidth, viewportHeight]
+    size: (context) => [context.viewportWidth, context.viewportHeight]
   },
 
   count: 3
